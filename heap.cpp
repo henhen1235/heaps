9f10adc45abf981cfd3c4627c02f4f183a592987
Henry Xu <henry.xu.aa.nl@gmail.com>
2025-02-01 16:21:38 -0800

    finished

--- heap.cpp
+++ heap.cpp
@@ -2,10 +2,10 @@
 #include "heap.h"
 #include <cstring>
 #include <math.h> 
-
+//imports
 using namespace std;
 
-heap::heap(){
+heap::heap(){//make the heap
   position = 1;
   heapar = new int[100];
   for(int x = 0; x < 100; x++) {
@@ -13,16 +13,16 @@ heap::heap(){
   }
 }
 
-heap::~heap(){
+heap::~heap(){//remove the heap
   delete[] heapar;
 }
 
-void heap::add(int value){
+void heap::add(int value){//adding new
   if (position >= 100){
     cout << "you are out of bounds";
   }
   else{
-    heapar[position] = value;
+    heapar[position] = value;// replace with the node above it if the current node is bigger
     int nPosition = position;
     while(nPosition != 1 && heapar[nPosition] > heapar[nPosition / 2]){
       int p1 = heapar[nPosition];
@@ -35,58 +35,88 @@ void heap::add(int value){
   }
 }
 
-int heap::removelargest(){//__________________________________________________________________________broken
+int heap::removelargest(){// remove largest
   if (position == 1){
     cout << "There are no nodes" << endl;
     return 0;
   }
-  int largest = heapar[1];
+  int largest = heapar[1];// get rid of largest and most latest node there
   int p1 = heapar[position];
   heapar[1] = p1;
   heapar[position] = 0;
   position = position - 1;
   int start = 1; 
-  while(heapar[start * 2] != 0){
-    if(heapar[start * 2 + 1] == 0){
-      if(heapar[start] < heapar[start * 2]){
-	 int templarge = heapar[start * 2];
-         int tempsmall = heapar[start];
-         heapar[start] = templarge;
-         heapar[start * 2] = tempsmall;
-         start = start * 2;
-	 break;
-      }
+  while (true){
+    int left = start * 2;
+    int right = start * 2 +1;
+    int larger = start;
+
+    if (left < position && heapar[left] > heapar[larger]){//if left is larger
+      larger = left;
     }
-    else{
-        if(heapar[start * 2 + 1] < heapar[start * 2]){
-          int templarge = heapar[start * 2]; 
-          int tempsmall = heapar[start];
-          heapar[start] = templarge;
-          heapar[start * 2] = tempsmall;
-          start = start * 2;
-        }
-	else if(heapar[start * 2 + 1] > heapar[start * 2]){
-          int templarge = heapar[start * 2 + 1];
-          int tempsmall = heapar[start];
-          heapar[start] = templarge;
-          heapar[start * 2 + 1] = tempsmall;
-          start = start * 2 + 1;
-	}
-	else{
-	  break;
-	}
+    
+    if (right < position && heapar[right] > heapar[larger]){// if right is larger
+      larger = right;
     }
-    }  
+
+    if (larger == start){
+      break;// if both are smaller
+    }
+    int tempsmaller = heapar[start];
+    int templarger = heapar[larger];//swap
+    heapar[start] = templarger;
+    heapar[larger] = tempsmaller;
+    start = larger;
+  }
+
+// this is an implementation method that I tried that did not work. Instead I just went back to what I was trying to do that the start and it worked.
+  // while(heapar[start * 2] != 0){
+  //   if(heapar[start * 2 + 1] == 0){
+  //     if(heapar[start] < heapar[start * 2]){
+	//  int templarge = heapar[start * 2];
+  //        int tempsmall = heapar[start];
+  //        heapar[start] = templarge;
+  //        heapar[start * 2] = tempsmall;
+  //        start = start * 2;
+	//  break;
+  //     }
+  //   }
+  //   else{
+  //       if(heapar[start * 2 + 1] < heapar[start * 2]){
+  //         int templarge = heapar[start * 2]; 
+  //         int tempsmall = heapar[start];
+  //         heapar[start] = templarge;
+  //         heapar[start * 2] = tempsmall;
+  //         start = start * 2;
+  //       }
+	// else if(heapar[start * 2 + 1] > heapar[start * 2]){
+  //         int templarge = heapar[start * 2 + 1];
+  //         int tempsmall = heapar[start];
+  //         heapar[start] = templarge;
+  //         heapar[start * 2 + 1] = tempsmall;
+  //         start = start * 2 + 1;
+	// }
+	// else{
+	//   if(heapar[start * 2 + 1] < heapar[start * 2]){
+  //         int templarge = heapar[start * 2]; 
+  //         int tempsmall = heapar[start];
+  //         heapar[start] = templarge;
+  //         heapar[start * 2] = tempsmall;
+  //         start = start * 2;
+  //       }
+	// }
+  //   }
+  //   }  
   return largest;
 }
 
-void heap::removeall(){
+void heap::removeall(){//run remove largest over and over until there is non left
   while(position > 1){
     removelargest();
   }
 }
 
-void heap::display(){
+void heap::display(){//diplay everything
   if (position == 1) {
     return;
   }
@@ -95,15 +125,18 @@ void heap::display(){
   displayrunner(start, depth, position);
 }
 
-void heap::displayrunner(int tempposition, int depth, int size){
-  if(tempposition * 2 + 1 < size && heapar[tempposition] != 0){
+void heap::displayrunner(int tempposition, int depth, int size){// display runner function
+  if(tempposition >= size || heapar[tempposition] == 0){
+    return;
+  }
+  if(tempposition * 2 + 1 < size && heapar[tempposition] != 0){//go down the right nodes
     displayrunner(tempposition * 2 + 1, depth + 1, size);
   }
-  for(int x = 0; x < depth; x++){
+  for(int x = 0; x < depth; x++){//orint
     cout << '\t';
   }
   cout << heapar[tempposition] << endl;
-  if(tempposition * 2 < size && heapar[tempposition] != 0){
+  if(tempposition * 2 < size && heapar[tempposition] != 0){//go down the left nodes
     displayrunner(tempposition *2, depth +1,size);
   }
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
 #include "heap.h"
+#include <cstring>
+#include <fstream>
+#include <vector>
+
 
 using namespace std;
 
 int main(){
-  cout << "hi" << endl;
+  heap* heaps = new heap;
+  while(true){
+    cout << "What would you like to do?" << endl;
+    char input[50];
+    char print[50] = "print";
+    char add[50] = "add";
+    char addfile[50] = "addfile";
+    char remove[50] = "remove";
+    char destroy[50] = "destroy";
+    char quit[50] = "quit";
+    cin >> input;
+    if (strcmp(input, add) == 0){//add whatever the user wants to add
+      cout << "Please enter an number" << endl;
+      int num;
+      cin >> num;
+      heaps->add(num);
+    }
+    else if (strcmp(input, addfile) == 0){//print out heap
+      cout << "Please type file name" << endl;
+      string name;
+      cin >> name;
+      string txt = ".txt";
+      ifstream inputFile(name + txt);
+      int num;
+      while (inputFile >> num) {
+        heaps->add(num);
+      }
+      inputFile.close();
+    }
+    else if (strcmp(input, print) == 0){//print out heap
+      heaps->display();
+    }
+    else if (strcmp(input, remove) == 0){//remove largest then print out
+      int largest = heaps->removelargest();
+      cout << largest << endl;
+    }
+    else if (strcmp(input, destroy) == 0){//destory enttire heap
+      while(heaps->position != 1){
+        int largest = heaps->removelargest();
+        cout << largest << endl;
+      }
+    }
+    else if (strcmp(input, quit) == 0){//quitting
+      delete heaps;
+      break;
+    }
+    else{
+     cout << "Your input was not valid your options are: add, addfile, print, remove, destory, quit.";//their command isn't valid
+        }
+  }
   return 0;
 }
